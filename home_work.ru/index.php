5ac7cdb34be8967d67851ef44e2e70e298abd48c
ivan1703 <iwan.balaschow228@mail.ru>
2017-10-26 17:40:33 +0300

    new file

---
+++ home_work.ru/index.php
@@ -0,0 +1,25 @@
+<!Doctype html>
+<html>
+<head>
+<title>Регестрация</title>
+<link rel="stylesheet" type="text/css" href="style/reset.css">
+<link rel="stylesheet" type="text/css" href="style/style.css">
+<meta charset='utf8'>	
+</head>
+<body>
+<div id="text_box">
+<h1>Все домашки.ру</h1>
+</div>
+<div id="block_reg">
+	<h1>Регистрация</h1>
+	<hr>
+	<Form name="reg" method="POST">
+		<input type="text" name="email" placeholder="Введите email..."><br>
+		<input type="text" name="password" placeholder="придумайте пароль..."><br>
+		<input type="text" name="password_1" placeholder="подтвердите пароль..."><br>
+		<input class="sub" type="submit" name="reg" value="Регистрация">
+	</Form>
+		<a href="">уже есть аккаунт?</a>
+</div>
+</body>
+</html>
