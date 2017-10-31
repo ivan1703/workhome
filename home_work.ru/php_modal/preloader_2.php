1d9a7b984e788791086f342abd02b8d80cf41efd
ivan1703 <iwan.balaschow228@mail.ru>
2017-10-31 22:28:50 +0300

    yesss
    Много нового и крутого

--- home_work.ru/php_modal/preloader_2.php
+++ home_work.ru/php_modal/preloader_2.php
@@ -2,5 +2,14 @@
 		<div class="loader">
 
 		</div>
+		<div class="loaders">
+			
+		</div>
+		<div class="loadersiv">
+			
+		</div>
+		<!-- <div class="char"> -->
+			<!-- <h1>I</h1> -->
+		<!-- </div> -->
 	</div>
 	<script type="text/javascript" src="../js/loader.js"></script>
